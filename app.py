44eface7d20b9382f44872b9b4b81e0968e9899f
Deepak R <deepakr948267@gmail.com>
2024-02-27 12:33:44 +0530

    added index.html

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False,port=8000)
+    # Use 0.0.0.0 as the host to make the app externally accessible
+    app.run(host='0.0.0.0', port=8000)
